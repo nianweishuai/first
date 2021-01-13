7a058e786e6bd5e21a703305c27eda1ef9bf8053
nianweishuai <ws-nian@logictech.cn>
2021-01-13 10:00:29 +0800

    test

---
+++ src/com/first/Main.java
@@ -0,0 +1,15 @@
+package com.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * 注释
+         */
+	    System.out.println("aaa");
+
+
+
+    }
+
+}
